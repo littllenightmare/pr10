6edfd46ff37aebec269160c4501c3836ebe1db3b
исп-31 <-31@LAB17-13>
2023-11-24 15:20:07 +0300

    Исправлен интерфейс

--- pr10/MainWindow.xaml.cs
+++ pr10/MainWindow.xaml.cs
@@ -24,68 +24,63 @@ namespace pr10
         {
             InitializeComponent();
             numbers = new ObservableCollection<int>();
-
         }
-        private void btnCalculate_click(object sender, RoutedEventArgs e)
+
+        private void TextCh(object sender, TextChangedEventArgs e)
         {
-            if (int.TryParse(tb_ot.Text, out int A) && int.TryParse(tb_do.Text, out int B))
+            calclb.Items.Clear();
+            numlb.Items.Clear();
+        }
+
+        private void infoClick(object sender, RoutedEventArgs e)
+        {
+          MessageBox.Show("Выполнено Кульковой А\r\nДан массив в диапазоне [-100;100] найти количество четных и нечетных. ");
+        }
+
+        private void CalcClick(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(ottb.Text, out int A) & int.TryParse(dotb.Text, out int B))
             {
                 if (B > A & A >= -100 & B <= 100)
                 {
-                    lab_intervel.Content = $"Числа от {A} до {B}";
                     int evenCount = 0;
                     int oddCount = 0;
-                    int[] array = (GenerateRandom.GenerateRandomArray(A, B));
-                    for (int i = 0; i < array.Length; i++)
+                    int[] mas = (GenerateRandom.GenerateRandomArray(A, B));
+                    for (int i = 0; i < mas.Length; i++)
                     {
-                        numbers.Add(array[i]);
+                        numbers.Add(mas[i]);
+                        numlb.Items.Add(mas[i]);
                     }
 
                     foreach (int number in numbers)
                     {
                         if (number % 2 == 0)
                         {
-                            evenCount++; // Увеличиваем счетчик четных чисел
+                            evenCount++;
                         }
                         else
                         {
-                            oddCount++; // Увеличиваем счетчик нечетных чисел
+                            oddCount++;
                         }
                     }
-                    for (int j = 0; j < array.Length; j++)
-                    {
-                        lb_num.Items.Add(array[j]);
-                    }
 
-                    lb_calc.Items.Add($"Количество четных чисел: {evenCount}\r\nКоличество нечетных чисел: {oddCount}");
+                    calclb.Items.Add($"Количество четных чисел: {evenCount}\r\nКоличество нечетных чисел: {oddCount}");
                 }
             }
         }
 
-        private void btnClear_Click(object sender, RoutedEventArgs e)
+        private void clearClick(object sender, RoutedEventArgs e)
         {
-            lb_num.Items.Clear();
-            lb_calc.Items.Clear();
-            tb_ot.Text = "";
-            tb_do.Text = "";
+            numlb.Items.Clear();
+            calclb.Items.Clear();
+            ottb.Text = "";
+            dotb.Text = "";
             numbers.Clear();
-            lab_intervel.Content = "";
         }
 
-        private void btnExit_Click(object sender, RoutedEventArgs e)
+        private void exClick(object sender, RoutedEventArgs e)
         {
             this.Close();
-        }
-
-        private void btnInfo_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Инфа");
-        }
-
-        private void TextCh(object sender, TextChangedEventArgs e)
-        {
-            lb_calc.Items.Clear();
-            lb_num.Items.Clear();
         }
     }
 }
